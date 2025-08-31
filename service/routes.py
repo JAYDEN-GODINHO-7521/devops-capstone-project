a70c5e37a4884f32c7d1556050e0975d9abf1d22
JAYDEN-GODINHO-7521 <godinhojayden4587@gmail.com>
2025-08-31 08:27:00 -0400

    update and delete

--- service/routes.py
+++ service/routes.py
@@ -92,14 +92,41 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_accounts(account_id):
+    """
+    Update a account
+
+    This endpoint will update a account based the body that is posted
+    """
+    app.logger.info("Request to Update a account with id [%s]", account_id)
+    account = Account.find(account_id)
+    if account is None:
+        return status.HTTP_404_NOT_FOUND
+    account.deserialize(request.get_json())
+    account.update()
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
 
-# ... place you code here to DELETE an account ...
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_accounts(account_id):
+    """
+    Delete a account
+
+    This endpoint will delete a account based the id specified in the path
+    """
+    app.logger.info("Request to Delete a account with id [%s]", account_id)
+
+    account = Account.find(account_id)
+    if account:
+        account.delete()
+
+    return "", status.HTTP_204_NO_CONTENT
+
 
 
 ######################################################################
